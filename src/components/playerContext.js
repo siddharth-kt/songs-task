5dca82ace3834d40bc0f1b25da49eef2a9856072
siddharth-kt <51018644+siddharth-kt@users.noreply.github.com>
2023-04-21 18:17:59 +0530

    Update playerContext.js

--- src/components/playerContext.js
+++ src/components/playerContext.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const PlayerContext = React.createContext({
   songs: [],
-  currentlyPlayingSong: {},
+  currentlyPlayingSong: null,
   setCurrentlyPlayingSong: () => {},
   moveNext: () => {},
   movePrevious: () => {},
